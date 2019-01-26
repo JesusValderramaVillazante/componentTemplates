2bc54e9367b583aa560018536281e089d62cb533
jesus <jesusValderramaVillazante@gmail.com>
2019-01-26 23:40:43 +0100

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'componentTemplates';
+  heroImageUrl = './assets/images/hero.png';
+  isSpecial: Boolean = true;
+
+  onSave(event: KeyboardEvent) {
+    const evtMsg = (<HTMLElement>event.target).textContent;
+    alert(evtMsg);
+    if (event) {
+      event.stopPropagation();
+    }
+  }
 }
